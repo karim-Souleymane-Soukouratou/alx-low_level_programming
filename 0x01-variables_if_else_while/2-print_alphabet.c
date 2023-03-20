28a86b07e46d10d384aeeea119723e73d34e193a
karim-Souleymane-Soukouratou <soukourasouleymane@gmail.com>
2023-03-20 05:03:51 -0700

    Task2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *entry int main
+ *description: print the alphabets in lowercase
+ *return success(0)
+ */
+
+int main(void)
+{
+	char var = 'a';
+	for (var = 'a'; var < 'z'; var++)
+	{
+		putchar(var);
+	}
+	putchar('\n');
+	return (0);
+}
