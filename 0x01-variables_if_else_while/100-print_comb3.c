6651adef3392735b8058875145e101734bfef2c3
karim-Souleymane-Soukouratou <soukourasouleymane@gmail.com>
2023-03-20 07:51:39 -0700

    Task10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Prints all possible different combinations of two digits.
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+	
+	for (i  = 0; i < 10; i++)
+	{
+		for (j = i + 1; j < 10; j++)
+		{
+			putchar(i + '0');
+			putchar(j + '0');
+
+			if (i < 8)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
